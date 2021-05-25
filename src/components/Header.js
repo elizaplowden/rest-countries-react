d93c03974a00f068fe40ccd3c5a46591a395a760
Eliza Plowden <elizaplowden@gmail.com>
2021-05-25 17:38:44 +0100

    .light-theme integration

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Header = () => {
   const changeTheme = () => {
-    const moon = document.querySelector('.fa-moon');
-    const header = document.querySelector('.header');
+    const moon = document.querySelector('.fa-moon')
+    const header = document.querySelector('.header')
 
-    const details = document.querySelectorAll('.details');
-    const uls = document.querySelectorAll('ul');
+    const details = document.querySelectorAll('.details')
+    const uls = document.querySelectorAll('ul')
 
     moon.addEventListener('click', () => {
-      document.body.classList.toggle('light-theme');
-      header.classList.toggle('light-theme');
+      document.body.classList.toggle('light-theme')
+      header.classList.toggle('light-theme')
 
       details.forEach((detail) => {
-        detail.classList.toggle('light-theme');
+        detail.classList.toggle('light-theme')
       })
 
       uls.forEach((ul) => {
-        ul.classList.toggle('light-theme');
+        ul.classList.toggle('light-theme')
       })
     });
 

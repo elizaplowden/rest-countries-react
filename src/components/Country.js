ac1ef3a45016338db6ed708bb75ef7393635b18d
Eliza Plowden <elizaplowden@gmail.com>
2021-05-25 13:23:37 +0100

    backlink from show page

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Country = () => {
   return (
     <div>
+      <Link to="/" className="btn btn-light">
+        <i className="fas fa-arrow-left"></i>Back Home
+      </Link>
       <h1>Country Data</h1>
     </div>
 

665df2282625ff19481c97234a6a637964541fd9
Eliza Plowden <elizaplowden@gmail.com>
2021-05-25 12:35:12 +0100

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Countries from './components/Countries';
 import Header from './components/Header';
 import Filter from './components/Filter';

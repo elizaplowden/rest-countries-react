0c125bebef5e7aa5cf3b4ac8c9f672050c3ba508
Eliza Plowden <elizaplowden@gmail.com>
2021-05-25 13:27:21 +0100

    api request for individual country

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const Country = () => {
+  const [country, setCountry] = useState([])
+  const {name} = useParams()
+
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      const response = await fetch(`https:restcountries.eu/rest/v2/name/${name}`);
+      const country = await response.json;
+      setCountry(country);
+    }
+  }, [])
+
+
   return (
     <div>
       <Link to="/" className="btn btn-light">

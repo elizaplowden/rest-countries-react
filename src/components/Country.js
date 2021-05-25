761b542bacada3470c8542638c10cc593151965a
Eliza Plowden <elizaplowden@gmail.com>
2021-05-25 16:42:00 +0100

    country card styling

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import '../country.css';
 
 const Country = () => {
   const [country, setCountry] = useState([])
@@ -17,13 +18,14 @@ const Country = () => {
 
 
   return (
-    <div>
-      <Link to="/" className="btn btn-light">
-        <i className="fas fa-arrow-left"></i>Back Home
-      </Link>
+    <>
       <section className="country">
+        <Link to="/" className="btn btn-light">
+          <i className="fas fa-arrow-left"></i>Back Home
+        </Link>
+
         {country.map((country) => {
-        {/*destructuring info from each country*/}
+        /*destructuring info from each country*/
           const {numericCode, flag, name, nativeName, population, region, subregion, capital, topLevelDomain, currencies, languages, borders} = country;
           return (
             <article key="numericCode">
@@ -46,10 +48,16 @@ const Country = () => {
                   <h5>Currencies: <span>{currencies[0].name}</span></h5>
                   <h5>Languages: <span>{languages[0].name}</span></h5>
                 </div>
-                <div>
-                  <h3>Border Countries: {borders}</h3>
-                </div>
-
+              </div>
+              <div className="borders">
+                <h3>Border Countries</h3>
+                {borders.map((border) => {
+                  return (
+                    <ul key={border}>
+                      <li>{border}</li>
+                    </ul>
+                  );
+                })}
               </div>
 
             </article>
@@ -57,7 +65,7 @@ const Country = () => {
 
         })}
       </section>
-    </div>
+    </>
 
   )
 }
